3a6a9fd5e7fdff69c0aca23e24ef8604915090d7
ZiyadT <ziyadtahlilkar@hotmail.com>
2021-05-18 15:17:39 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -38,25 +38,24 @@ image_byt = urlopen("https://upload.wikimedia.org/wikipedia/commons/d/d7/UFC_Log
 image_b64 = base64.encodebytes(image_byt)
 logo.config(data=image_b64)
 
-logo = logo.zoom(23)
-logo = logo.subsample(310)
+logo = logo.subsample(int(0.026*window_width))
 
 logo_pic = frame.create_image(100, 100, image=logo, anchor='ne')
 frame.move(logo_pic, int(0.44*window_width), int(-0.1169*window_height))
 
 subheading = tk.Label(frame, text='Fighter Statistics', bg='gray12', fg='white')
-subheading.config(font=("Courier", 15, "bold"))
+subheading.config(font=("Courier", int(0.03*window_width), "bold"))
 subheading.place(x=int(0.28*window_width), y=int(0.078*window_height))
 
 label = tk.Label(frame, text='Fighter Name', bg='gray12', fg='white')
-label.config(font=("Courier", 10, "bold"))
+label.config(font=("Courier", int(0.02*window_width), "bold"))
 label.place(x=int(0.02*window_width), y=int(0.941*window_height))
 
-entry_selection = tk.Entry(frame, width=30, font=("Courier 12"))
+entry_selection = tk.Entry(frame, width=int(0.06*window_width), font=(f"Courier {int(0.024*window_width)}"))
 entry_selection.bind('<Return>', enter)
 entry_selection.place(x=int(0.24*window_width), y=int(0.941*window_height))
 
-enter_button = tk.Button(frame, width=4, height=1, text='>', command=enter)
+enter_button = tk.Button(frame, width=int(0.008*window_width), height=1, text='>', command=enter)
 enter_button.place(x=int(0.894*window_width), y=int(0.940*window_height))
 
 root.mainloop()
